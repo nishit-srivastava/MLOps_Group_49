3af48cab5be0284f8e049ccf00cfac21d03d01b2
gaurav_vivek <2023ac05362@wilp.bits-pilani.ac.in>
2025-08-10 21:49:21 +0530

    chore: add model_v11.pkl

--- retraining/retrain.py
+++ retraining/retrain.py
@@ -245,7 +245,7 @@ print(f"Created new model: {new_model_path}")
 # Add and commit to git
 subprocess.run(["git", "add", "."], check=True)
 subprocess.run(["git", "commit", "-m", f"chore: add model_v{new_version}.pkl"], check=True)
-#subprocess.run(["git", "pull", "--rebase"])
+subprocess.run(["git", "pull", "--rebase"])
 subprocess.run(["git", "push"], check=True)
 
 
