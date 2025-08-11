1327f8ec843663b16a546147902ead90162c188b
Nishit <nisgin@live.com>
2025-08-11 14:21:30 +0530

    cleaned up unused folder and files

--- app/utils/prometheus.py
+++
@@ -1,7 +0,0 @@
-from prometheus_client import Counter, Histogram, generate_latest
-
-prediction_counter = Counter("predictions_total", "Total number of prediction requests")
-prediction_latency = Histogram("prediction_latency_seconds", "Time spent on prediction")
-
-def get_metrics():
-    return generate_latest()
